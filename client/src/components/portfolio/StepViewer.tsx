72ce12cc16ce47d8c276c4a99fc9979e2b8d487e
jessieakdogan <jessieakdogan@gmail.com>
2025-08-04 15:46:52 +0100

    first commit

--- client/src/components/portfolio/StepViewer.tsx
+++ client/src/components/portfolio/StepViewer.tsx
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
 import { FileText } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, ContactShadows } from '@react-three/drei'
-import Loader  from './Loader'
+import { Canvas } from "@react-three/fiber";
+import Loader from "./Loader";
 
 
 const StepViewer = ({ file, fileType }: { file: string; fileType: string }) => {
-  const [ready, setReady] = useState(false)
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    setReady(false)
-  }, [file])
+    setReady(false);
+  }, [file]);
 
   return (
     <Card className="bg-white/80 backdrop-blur-sm border-gray-200">
@@ -22,18 +21,15 @@ const StepViewer = ({ file, fileType }: { file: string; fileType: string }) => {
         </CardTitle>
       </CardHeader>
       <CardContent>
-       <div className="w-full h-[200px] xxs:h-[250px]  xs:h-[300px] sm:h-[400px] max-h-[640px] overflow-hidden bg-gray-100 rounded-lg flex items-center justify-center border-2 border-dashed border-gray-300 relative">
+        <div className="w-full h-[200px] xxs:h-[250px]  xs:h-[300px] sm:h-[400px] max-h-[640px] overflow-hidden bg-gray-100 rounded-lg flex items-center justify-center border-2 border-dashed border-gray-300 relative">
           {!ready && <Loader progress={null} />}
-          <Canvas 
+          <Canvas camera={{ position: [5, 5, 5], fov: 45 }} shadows>
          
-          camera={{ position: [5, 5, 5], fov: 45 }} 
-          shadows>
-           
           </Canvas>
         </div>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default StepViewer
+export default StepViewer;
