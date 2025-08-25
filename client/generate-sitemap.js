c79620b2da5c34676fdccdee22c2dd23a57edc8e
jessieakdogan <jessieakdogan@gmail.com>
2025-08-25 16:15:34 +0100

    first commit

---
+++ client/generate-sitemap.js
@@ -0,0 +1,47 @@
+// client/generate-sitemap.js
+const fs = require("fs");
+const path = require("path");
+
+// Change this to your production site
+const BASE_URL = import.meta.env.VITE_CLIENT_URL || "https://inkaer.com";
+
+// List out your static routes
+const routes = [
+  "/", 
+  "/about",
+  "/careers",
+  "/blog",
+  "/contact",
+  "/terms",
+  "/privacy",
+  "/sign-in",
+  "/sign-up",
+  "/forgot-password",
+  "/reset-password",
+  "/unauthorized",
+];
+
+function generateSitemap() {
+  const urls = routes.map(route => {
+    return `
+  <url>
+    <loc>${BASE_URL}${route}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  }).join("\n");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  const publicDir = path.join(__dirname, "public");
+  if (!fs.existsSync(publicDir)) {
+    fs.mkdirSync(publicDir);
+  }
+  fs.writeFileSync(path.join(publicDir, "sitemap.xml"), sitemap, "utf8");
+  console.log(" Sitemap generated at client/public/sitemap.xml");
+}
+
+generateSitemap();
