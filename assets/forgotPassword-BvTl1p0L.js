de48c1ecc1f44803645b776bfc62c9a21a29c675
Jessica-akdogan <jessica.akdogan@3onsconnect.com>
2025-09-09 08:33:32 +0000

    Build: (b654fd5) first commit

---
+++ assets/forgotPassword-BvTl1p0L.js
@@ -0,0 +1,1 @@
+import{K as e,M as o,N as t,a}from"./index-DgZA0KzG.js";const w=async s=>{await e(a,s,{url:`${window.location.origin}/reset-password`})},n=async({oobCode:s,newPassword:r})=>{await o(a,s),await t(a,s,r)};export{n as c,w as r};
--- assets/useBlog-B44WE6Q9.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./useQuery-BxWVBd8f.js";import{c as r,g as s,f as t}from"./blogService-KIPBbKyZ.js";const g=()=>o({queryKey:["blogHeader"],queryFn:r}),l=()=>o({queryKey:["blogPosts"],queryFn:s}),n=e=>o({queryKey:["blogPost",e],queryFn:()=>t(e),enabled:!!e,initialData:null});export{l as a,n as b,g as u};
